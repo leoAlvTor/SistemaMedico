7ca0e98c060c6e4f3036a03b6c7e51e86fdddf6e
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-06-29 09:27:40 -0500

    New methods implemented: - createRecord: Receive an instance of Paciente to insert into database (Will be replaced with common dbutils). - getRecordById: Receive an id (object) to query a record based on it id. - getAll: Return all records from database. - closeConnection: As its name suggest close DB connection.

--- src/controller/PacienteController.java
+++ src/controller/PacienteController.java
@@ -1,26 +1,73 @@
 package controller;
 
 import model.Paciente;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.*;
 
 public class PacienteController extends CRUD<Paciente> {
 
-    @Override
-    public boolean createRecord(Paciente instance) {
+    private Connection connection;
 
-        return false;
+    public PacienteController(){
+        this.connection = DBConnection.getConnection();
+    }
+
+    @Override
+    public boolean createRecord(Paciente instance){
+        var sql = "insert into paciente(CEDULA, NOMBRES, APELLIDOS, DIRECCION, TELEFONO, CELULAR, ESTADOCIVIL, " +
+                "PROCEDENCIA, RESIDENCIA, FECHANACIMIENTO, GENERO, ANTECEDENTES, PESO, TALLA, GRUPOSANGUINEO) values " +
+                "(?, ?, ?, ?, ?, ?," +
+                " ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            List<Object> objectList = instance.mapToList();
+            for (int i = 0; i < objectList.size(); i++) {
+                if(!Objects.isNull(objectList.get(i)))
+                    preparedStatement.setObject(i + 1, instance.mapToList().get(i));
+                else
+                    preparedStatement.setObject(i, "NA");
+            }
+            boolean isCorrect = preparedStatement.execute();
+            preparedStatement.close();
+            return isCorrect;
+        }catch (Exception e){
+            System.err.println("ERROR: Error while creating Paciente.");
+            return false;
+        }
     }
 
     @Override
     public Paciente getRecordById(Object id) {
-        return null;
+        ResultSetHandler<Paciente> resultSetHandler = new BeanHandler<>(Paciente.class);
+        var runner = new QueryRunner();
+        try {
+            return runner.query(connection, "select * from paciente where NUMEROFICHA = ?", resultSetHandler
+                    , id);
+        }catch (Exception e){
+            System.out.println("ERROR: Error while getting Paciente record with ID: " + id);
+            return null;
+        }
     }
 
     @Override
-    public List<Paciente> getAll() {
-        return null;
+    public List<Paciente> getAll(){
+        try {
+            BeanListHandler<Paciente> beanListHandler = new BeanListHandler<>(Paciente.class);
+            var runner = new QueryRunner();
+            return runner.query(connection, "select * from paciente", beanListHandler);
+        }catch (Exception e){
+            System.out.println("ERROR: Error while getting all records from Paciente");
+            return new ArrayList<>();
+        }
     }
 
     @Override
@@ -33,5 +80,23 @@ public class PacienteController extends CRUD<Paciente> {
         return false;
     }
 
+    public boolean closeConnection(){
+        try {
+            if (!Objects.isNull(this.connection) && !this.connection.isClosed()) {
+                this.connection.close();
+                return true;
+            }else{
+                return false;
+            }
+        }catch (Exception e){
+            System.out.println("ERROR: Error while closing the connection.");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        PacienteController pacienteController = new PacienteController();
+
+    }
 
 }
