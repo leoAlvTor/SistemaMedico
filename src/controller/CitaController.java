d7b44672123ac9450616d2b563af1d4aab8ff98c
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-06-29 09:23:25 -0500

    Under development (No important changes).

--- src/controller/CitaController.java
+++ src/controller/CitaController.java
@@ -2,17 +2,28 @@ package controller;
 
 import model.Cita;
 
+import javax.swing.*;
+import java.sql.Connection;
 import java.util.List;
+import java.util.Objects;
 
 public class CitaController extends CRUD<Cita>{
 
+    private Connection connection;
+    private Cita cita;
+
     public CitaController(){
-
+        connection = DBConnection.getConnection();
+        cita = new Cita();
+        if(Objects.isNull(connection))
+            JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos.");
     }
-
 
     @Override
     public boolean createRecord(Cita instance) {
+        String sql = "INSERT INTO CITA VALUES(?,?,?,?,?,?,?,?)";
+
+
         return false;
     }
 
