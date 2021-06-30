0427dfb5e071c0ae858e9af06981b36fc73bff53
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-06-30 11:15:33 -0500

    Table model for Cita model.

---
+++ src/model/CitaTableModel.java
@@ -0,0 +1,22 @@
+package model;
+
+import javax.swing.table.AbstractTableModel;
+
+public class CitaTableModel extends AbstractTableModel {
+
+
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
+}
