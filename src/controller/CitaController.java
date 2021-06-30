3a1214dd48b0a1eb04967ba1bf714b69889f0482
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-06-30 09:36:06 -0500

    CRUD method implementation done in CitaController.java. New method for getting all records based on foreign key.

--- src/controller/CitaController.java
+++ src/controller/CitaController.java
@@ -1,50 +1,99 @@
 package controller;
 
 import model.Cita;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import javax.swing.*;
 import java.math.BigInteger;
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class CitaController extends CRUD<Cita>{
 
     private Connection connection;
-    private Cita cita;
 
     public CitaController(){
         connection = DBConnection.getConnection();
-        cita = new Cita();
         if(Objects.isNull(connection))
             JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos.");
     }
 
     @Override
     public BigInteger createRecord(Object ... objects) {
-        String sql = "INSERT INTO CITA VALUES(?,?,?,?,?,?,?,?)";
-
-
-        return BigInteger.ONE;
+        String sql = "insert into cita(NUMEROFICHA, FECHA, ANAMNESIS, RECETA, DIAGNOSTICO, EXAMENES) values (?, ?, ?," +
+                " ?, ?, ?)";
+        try{
+            ScalarHandler<BigInteger> scalarHandler = new ScalarHandler<>();
+            return new QueryRunner().insert(connection, sql, scalarHandler, objects);
+        }catch (Exception e){
+            System.out.println("ERROR: Error while creating record in CITA");
+            return BigInteger.valueOf(-1);
+        }
     }
 
     @Override
     public Cita getRecordById(Object id) {
-        return null;
+        ResultSetHandler<Cita> resultSetHandler = new BeanHandler<>(Cita.class);
+        try{
+            return new QueryRunner().query(connection, "select * from cita where NUMEROREGISTRO = ?",
+                    resultSetHandler, id);
+        }catch (Exception e){
+            System.out.println("ERROR: Error while getting Cita record with ID: " + id);
+            return null;
+        }
     }
 
     @Override
     public List<Cita> getAll() {
-        return null;
+        try{
+            BeanListHandler<Cita> beanListHandler = new BeanListHandler<>(Cita.class);
+            return new QueryRunner().query(connection, "select * from cita", beanListHandler);
+        }catch (Exception e){
+            System.out.println("ERROR: Error while getting all records from Cita");
+            return new ArrayList<>();
+        }
+    }
+
+    public List<Cita> getAllByPaciente(int numeroFicha){
+        try{
+            BeanListHandler<Cita> beanListHandler = new BeanListHandler<>(Cita.class);
+            return new QueryRunner().query(connection, "select * from cita where NUMEROFICHA = ?", beanListHandler,
+                    numeroFicha);
+        }catch (Exception e){
+            System.out.println("ERROR: Error while getting all records by NUMEROFICHA = " + numeroFicha);
+            return new ArrayList<>();
+        }
     }
 
     @Override
     public int updateRecord(Object id, Object ... objects) {
-        return -1;
+        Object[] objects1 = new Object[objects.length+1];
+        System.arraycopy(objects, 0, objects1, 0, objects.length);
+        objects1[objects.length] = id;
+        try{
+            String updateSQL = "update cita set NUMEROFICHA = ?, FECHA = ?, ANAMNESIS = ?, RECETA = ?, DIAGNOSTICO = " +
+                    "?, EXAMENES = ? where NUMEROREGISTRO = ?";
+            return new QueryRunner().update(connection, updateSQL, objects1);
+        }catch (Exception e){
+            System.out.println("ERROR: Error while updating record with id: " + id);
+            return -1;
+        }
     }
 
     @Override
     public int deleteRecord(Object id) {
-        return -1;
+        try{
+            var sql = "delete from cita where NUMEROREGISTRO = ?";
+            return new QueryRunner().update(connection, sql, id);
+        }catch (Exception e){
+            System.out.println("ERROR: Error while deleting Cita record with id: " + id);
+            return -1;
+        }
     }
 }
