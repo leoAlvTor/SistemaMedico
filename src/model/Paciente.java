b421893062df73b78d1b86e06fecb31de00afe86
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-07-09 15:29:00 -0500

    Method to verify cedula.

--- src/model/Paciente.java
+++ src/model/Paciente.java
@@ -268,6 +268,37 @@ public class Paciente {
                 new org.joda.time.LocalDate(new Date())).getDays()/365;
     }
 
+    public boolean verifyID(){
+        byte sum = 0;
+        try {
+            if (cedula.trim().length() != 10)
+                return false;
+            String[] data = cedula.split("");
+            byte verifier = Byte.parseByte(data[0] + data[1]);
+            if (verifier < 1 || verifier > 24)
+                return false;
+            byte[] digits = new byte[data.length];
+            for (byte i = 0; i < digits.length; i++)
+                digits[i] = Byte.parseByte(data[i]);
+            if (digits[2] > 6)
+                return false;
+            for (byte i = 0; i < digits.length - 1; i++) {
+                if (i % 2 == 0) {
+                    verifier = (byte) (digits[i] * 2);
+                    if (verifier > 9)
+                        verifier = (byte) (verifier - 9);
+                } else
+                    verifier = (byte) (digits[i]);
+                sum = (byte) (sum + verifier);
+            }
+            if ((sum - (sum % 10) + 10 - sum) == digits[9])
+                return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
