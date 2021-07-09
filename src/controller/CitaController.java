0771975bdce481383c6dc04f8be78d7ddf081c81
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-07-09 12:25:00 -0500

    CitaController.java implements Interface instead of extend an abstract class.

--- src/controller/CitaController.java
+++ src/controller/CitaController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class CitaController extends CRUD<Cita>{
+public class CitaController implements CRUD<Cita>{
 
     private Connection connection;
 
