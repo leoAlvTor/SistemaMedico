3c1540641f372e53947c2a386832c4bcf165833c
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-07-09 12:27:01 -0500

    DBConnection.java is documented.

--- src/controller/DBConnection.java
+++ src/controller/DBConnection.java
@@ -3,14 +3,28 @@ package controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+/**
+ * Class for database connection.
+ */
 public class DBConnection {
+    // Constructor.
+    public DBConnection(){}
 
+    // Class name connection.
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    // Database URL.
     private static final String DB_URL = "jdbc:mysql://localhost:3306/sistemamedico";
-
+    // Database user.
     private static final String USER = "usuario_seguro";
+    // Database password.
     private static final String PASSWORD = "pass2021word";
 
+    /**
+     * Static method for creating a new connection to database.
+     *
+     * @return a Connection instance.
+     * If any error occurs return null instead of Connection.
+     */
     public static Connection getConnection(){
         try{
 
