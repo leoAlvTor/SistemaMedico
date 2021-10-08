fdfdadcd8bd2fdff5ca7fc764d8da692b4625e0f
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-10-08 09:22:37 -0500

    New table column order.

--- src/model/PacienteTableModel.java
+++ src/model/PacienteTableModel.java
@@ -2,7 +2,9 @@ package model;
 
 import javax.swing.table.AbstractTableModel;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Class to define table model for Java Swing.
@@ -10,14 +12,9 @@ import java.util.List;
  */
 public class PacienteTableModel extends AbstractTableModel {
 
-    private final String[] columnas = {"N. FICHA", "CEDULA", "NOMBRE", "APELLIDO", "DIRECCION", "TELEFONO", "CELULAR",
-            "ESTADO CIVIL", "PROCEDENCIA", "RECIDENCIA", "FECHA NACIMIENTO", "GENERO", "ANTECEDENTES", "PESO", "TALLA",
-            "GRUPO SANGUINEO"};
+    private final String[] columnas = {"N. FICHA", "CEDULA", "APELLIDO", "NOMBRE"};
 
-    public Class<?>[] tiposColumnas = {String.class, String.class, String.class, String.class, String.class,
-            String.class,
-            String.class, String.class, String.class, String.class, String.class, String.class, String.class,
-            String.class, String.class, String.class};
+    public Class<?>[] tiposColumnas = {String.class, String.class, String.class, String.class};
 
     private List<Paciente> pacienteList;
 
@@ -28,6 +25,9 @@ public class PacienteTableModel extends AbstractTableModel {
 
     public PacienteTableModel(List<Paciente> pacientes){
         super();
+        if(pacienteList != null)
+            this.pacienteList.stream()
+                .sorted(Comparator.comparing(Paciente::getApellidos)).collect(Collectors.toList());
         this.pacienteList = pacientes;
     }
 
@@ -59,47 +59,12 @@ public class PacienteTableModel extends AbstractTableModel {
                 paciente.setCedula((String) aValue);
                 break;
             case 2:
-                paciente.setNombres((String) aValue);
-                break;
-            case 3:
                 paciente.setApellidos((String) aValue);
                 break;
-            case 4:
-                paciente.setDireccion((String) aValue);
+            case 3:
+                paciente.setNombres((String) aValue);
                 break;
-            case 5:
-                paciente.setTelefono((String) aValue);
-                break;
-            case 6:
-                paciente.setCelular((String) aValue);
-                break;
-            case 7:
-                paciente.setEstadoCivil((String) aValue);
-                break;
-            case 8:
-                paciente.setProcedencia((String) aValue);
-                break;
-            case 9:
-                paciente.setResidencia((String) aValue);
-                break;
-            case 10:
-                paciente.setFechaNacimiento((String) aValue);
-                break;
-            case 11:
-                paciente.setGenero((String) aValue);
-                break;
-            case 12:
-                paciente.setAntecedentes((String) aValue);
-                break;
-            case 13:
-                paciente.setPeso((double) aValue);
-                break;
-            case 14:
-                paciente.setTalla((double) aValue);
-                break;
-            case 15:
-                paciente.setGrupoSanguineo((String) aValue);
-                break;
+
             default:
                 break;
         }
@@ -116,35 +81,18 @@ public class PacienteTableModel extends AbstractTableModel {
             case 1:
                 return paciente.getCedula();
             case 2:
-                return paciente.getNombres();
-            case 3:
                 return paciente.getApellidos();
-            case 4:
-                return paciente.getDireccion();
-            case 5:
-                return paciente.getTelefono();
-            case 6:
-                return paciente.getCelular();
-            case 7:
-                return paciente.getEstadoCivil();
-            case 8:
-                return paciente.getProcedencia();
-            case 9:
-                return paciente.getResidencia();
-            case 10:
-                return paciente.getFechaNacimiento();
-            case 11:
-                return paciente.getGenero();
-            case 12:
-                return paciente.getAntecedentes();
-            case 13:
-                return paciente.getPeso();
-            case 14:
-                return paciente.getTalla();
-            case 15:
-                return paciente.getGrupoSanguineo();
+            case 3:
+                return paciente.getNombres();
+
             default:
                 return "";
         }
     }
+
+    @Override
+    public String getColumnName(int column){
+        return columnas[column];
+    }
+
 }
